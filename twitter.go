e280e94066a7fc52955fdf690b4f8dc7cd1285cb
Splerge <damnsplurge@gmail.com>
2016-10-17 15:47:12 +1100

    Added Datastore Support

--- twitter.go
+++ twitter.go
@@ -12,6 +12,7 @@ import (
 	"strings"
 	//"oauth"
 	"appengine"
+	"appengine/datastore"
 	"appengine/urlfetch"
 	b64 "encoding/base64"
 	"encoding/json"
@@ -36,6 +37,7 @@ type Token struct {
 type TwitterResponse struct {
 	Statuses []struct {
 		Text string `json:"text"`
+		Id string `json:"id_str"`
 		Geo  struct {
 			Coordinates []float64 `json:"coordinates"`
 		} `json:"geo"`
@@ -57,19 +59,23 @@ type Coordinates struct {
 	Latitude  float64
 }
 
+type GeoPoint struct {
+	Lng float64	
+	Lat  float64
+}
+
+type TweetData struct {
+	Content string
+	Gp GeoPoint
+}
+
 func root(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintf(w, "<html><title>TweetMap</title>")
-	testkeyword := "Fitzroy"
-	tweetArray := new(TwitterResponse)
-	access_token, success := checkForAccessToken()
-	if success == false {
-		access_token = authorise("L23T9SJUKk4zGrZf0lGjhXQZV", "i7mCRyxSMUc1uS8c4EGGcZWM47gDTDOxNwE6PvURTCQIQlhi5f", w, r)
-	}
-	requestKeyword(testkeyword, access_token, w, r, tweetArray)
+	//testkeyword := "Fitzroy"
 	//fmt.Fprintf(w, "<p>%s</p>", access_token)
 	fmt.Fprintf(w, "<!DOCTYPE html><html>")
-	heatMapPage(w, r, requestKeyword(testkeyword, access_token, w, r, tweetArray))
+	heatMapPage(w, r, nil)
 	fmt.Fprintf(w, "</html>")
 }
 
@@ -86,6 +92,10 @@ func submit(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintf(w, "<p>%s</p>", access_token)
 	heatMapPage(w, r, requestKeyword(keyword, access_token, w, r, tweetArray))
 	fmt.Fprintf(w, "</html>")
+}
+
+func tweetStorageKey(c appengine.Context) *datastore.Key {
+	return datastore.NewKey(c, "TweetStorage", "default_tweetstorage", 0, nil)
 }
 
 func authorise(consumerkey string, consumersecretkey string, w http.ResponseWriter, r *http.Request) string {
@@ -131,19 +141,11 @@ func getCoords(coords []Coordinates) string {
 	}
 	s := ""
 	for _, v := range coords {
-		s = s + ("new google.maps.LatLng(" + strconv.FormatFloat(v.Latitude, 'f', -1, 64) + ", " + strconv.FormatFloat(v.Longitude, 'f', -1, 64) + "),\n")
-	}
-	return s
-
-}
-
-func getCoords(coords []Coordinates) string {
-	if coords == nil {
-		return ""
-	}
-	s := ""
-	for _, v := range coords {
-		s = s + ("new google.maps.LatLng(" + strconv.FormatFloat(v.Latitude, 'f', -1, 64) + ", " + strconv.FormatFloat(v.Longitude, 'f', -1, 64) + "),\n")
+		if (v.Latitude == 0 && v.Longitude == 0) {
+			// skip bad values
+		} else {
+			s = s + ("new google.maps.LatLng(" + strconv.FormatFloat(v.Latitude, 'f', -1, 64) + ", " + strconv.FormatFloat(v.Longitude, 'f', -1, 64) + "),\n") 
+		}
 	}
 	return s
 
@@ -171,14 +173,14 @@ func requestKeyword(keyword string, accesstoken string, w http.ResponseWriter, r
 			fmt.Fprintf(w, "<p>%+v</p>", twitterResp)
 			//fmt.Fprintf(w, "<p>%d</p>", len(twitterResp.Statuses))
 			//return
-			return compileLocationResults(&twitterResp, w, keyword)
+			return compileLocationResults(&twitterResp, w, r, keyword)
 		}
 		//fmt.Fprintf(w, "<p>%s</p>", body)
 	}
 	return nil
 }
 
-func compileLocationResults(twitterResp *TwitterResponse, w http.ResponseWriter, keyword string) []Coordinates {
+func compileLocationResults(twitterResp *TwitterResponse, w http.ResponseWriter, r *http.Request, keyword string) []Coordinates {
 	i := 0
 	coordSlice := make([]Coordinates, 1)
 	for _, v := range twitterResp.Statuses {
@@ -190,8 +192,11 @@ func compileLocationResults(twitterResp *TwitterResponse, w http.ResponseWriter,
 					Latitude:  v.Geo.Coordinates[0],
 					Longitude: v.Geo.Coordinates[1],
 				}
-				coordSlice = append(coordSlice, bufferCoords)
-				i = i + 1
+				if bufferCoords.Latitude != 0 && bufferCoords.Longitude != 0 {			
+					storeTweet(v.Id, v.Text, bufferCoords, w, r)
+					coordSlice = append(coordSlice, bufferCoords)
+					i = i + 1
+				}
 			} else if len(v.Place.Bounds.Coordinates) >= 1 {
 				bufferCoords := Coordinates{
 					Latitude:  v.Place.Bounds.Coordinates[0][0][0],
@@ -209,18 +214,74 @@ func compileLocationResults(twitterResp *TwitterResponse, w http.ResponseWriter,
 					Latitude:  v.Place.Bounds.Coordinates[0][3][0],
 					Longitude: v.Place.Bounds.Coordinates[0][3][1],
 				}
-				coordSlice = append(coordSlice, bufferCoords)
+				if bufferCoords.Latitude != 0 && bufferCoords.Longitude != 0 {			
+					storeTweet(v.Id, v.Text, bufferCoords, w, r)
+					coordSlice = append(coordSlice, bufferCoords)
+					i = i + 1
+				}
+				if bufferCoords2.Latitude != 0 && bufferCoords2.Longitude != 0 {			
+					storeTweet(v.Id, v.Text, bufferCoords2, w, r)
+					coordSlice = append(coordSlice, bufferCoords2)
+					i = i + 1
+				}
 
-				coordSlice = append(coordSlice, bufferCoords2)
+				if bufferCoords3.Latitude != 0 && bufferCoords3.Longitude != 0 {			
+					storeTweet(v.Id, v.Text, bufferCoords3, w, r)
+					coordSlice = append(coordSlice, bufferCoords3)
+					i = i + 1
+				}
 
-				coordSlice = append(coordSlice, bufferCoords3)
-
-				coordSlice = append(coordSlice, bufferCoords4)
+				if bufferCoords4.Latitude != 0 && bufferCoords4.Longitude != 0 {			
+					storeTweet(v.Id, v.Text, bufferCoords4, w, r)
+					coordSlice = append(coordSlice, bufferCoords4)
+					i = i + 1
+				}
 			}
 		}
 	}
-	fmt.Fprintf(w, "%+v", coordSlice)
+	coordSlice = append(coordSlice, getStoredData(w, r, keyword)...)
+	
+	//fmt.Fprintf(w, "%+v", coordSlice)
 	return coordSlice
+}
+
+func getStoredData(w http.ResponseWriter, r *http.Request, keyword string) []Coordinates {
+	c := appengine.NewContext(r)
+	coordSlice := make([]Coordinates, 1)
+	query := datastore.NewQuery("TweetData").Ancestor(tweetStorageKey(c)).Limit(200)
+	tweets := make([]TweetData, 1)
+	if _, err := query.GetAll(c, &tweets); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+            return nil
+	}
+	for _,v := range tweets {
+		if strings.Contains(v.Content, keyword) {
+			bufferCoords := Coordinates{
+				Latitude:  v.Gp.Lat,
+				Longitude: v.Gp.Lng,
+			}
+			coordSlice = append(coordSlice, bufferCoords)
+		}
+	}
+	return coordSlice
+}
+
+func storeTweet(tweetid string, content string, coords Coordinates, w http.ResponseWriter,  r *http.Request) {
+	c := appengine.NewContext(r)
+	newGP := GeoPoint{
+		Lat: coords.Latitude,
+		Lng: coords.Longitude,
+	}
+	data := TweetData{
+		Content: content,
+		Gp: newGP,
+	}
+	key := datastore.NewKey(c, "TweetStorage", tweetid, 0, nil)
+
+	_, err := datastore.Put(c, key, &data)
+	if err != nil {
+		fmt.Fprintf(w, "<p>%s</p>", err)
+	}
 }
 
 func checkForAccessToken() (string, bool) { /* TODO */
